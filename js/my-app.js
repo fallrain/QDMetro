f9e1536f48bea5e8ff8a2ec18ee21322d7945295
王广开 <416266475@qq.com>
2017-12-21 10:53:35 +0800

    新需求

--- js/my-app.js
+++ js/my-app.js
@@ -109,6 +109,94 @@ myApp.onPageInit('moreFun', function (page) {
 myApp.onPageInit('find-list', function (page) {
 
 });
+
+//路线
+var lineObj = {};
+myApp.onPageInit('line', function (page) {
+  var zhanAy = ['青岛站', '人民会堂', '汇泉广场', '中山公园', '太平角公园', '延安三路', '五四广场', '江西路', '宁夏路', '敦化路', '错埠岭', '清江路', '双山', '长沙路', '地铁大厦', '海尔路', '万年泉路', '李村', '群峰路', '振华路', '永平路', '青岛北站'];
+  var curStationName = '';
+  $('.z').click(function () {
+    var lineTip = $('.js-lineTip');
+    var lw = lineTip.width();
+    var lh = lineTip.height();
+    var pos = $(this).position();
+    var w = $('.line-map').width();
+    lineTip.css({
+      top: (pos.top - lh * 0.9) * 100 / w + 'vw',
+      left: (pos.left - lw * 0.79) * 100 / w + 'vw'
+    });
+    lineTip.show();
+    var curStationIndex = $(this).attr('data-i');
+    curStationName = zhanAy[curStationIndex];
+  });
+  $('.js-qd').click(function () {
+    $('.js-ipt-qd').val(curStationName);
+    var btmzm = curStationName;
+    lineObj.qd = curStationName;
+    if ($('.js-ipt-zd').val()) {
+      btmzm += '-' + $('.js-ipt-zd').val();
+    }
+    $('.js-btm-zm').text(btmzm);
+    if ($('.js-ipt-zd').val().replace('/\s/g', '')) {
+      mainView.router.load({
+        url: 'line-detail.html'
+      });
+    }
+  });
+
+  $('.js-zd').click(function () {
+    $('.js-ipt-zd').val(curStationName);
+    var btmzm = curStationName;
+    lineObj.zd = curStationName;
+    if ($('.js-ipt-qd').val()) {
+      btmzm = $('.js-ipt-qd').val() + '-' + btmzm;
+    }
+    $('.js-btm-zm').text(btmzm);
+    if ($('.js-ipt-qd').val().replace('/\s/g', '')) {
+      mainView.router.load({
+        url: 'line-detail.html'
+      })
+    }
+  });
+});
+//线路列表
+myApp.onPageInit('line-list', function (page) {
+  //点击商品，跳转商品详情
+  $('.product-list-item').on('click', function () {
+    mainView.router.load({
+      url: 'product-detail.html'
+    })
+  });
+  var items = $('.line-list-item');
+  items.on('click', '.start', function () {
+    items.removeClass('startActive');
+    var $this = $(this);
+    var item = $this.closest('.line-list-item');
+    item.removeClass('endActive');
+    item.addClass('startActive');
+    lineObj.qd = item.find('.mid').text();
+    if ($('.endActive').length && $('.startActive').length) {
+      mainView.router.load({
+        url: 'line-detail.html'
+      })
+    }
+  });
+
+  items.on('click', '.end', function () {
+    items.removeClass('endActive');
+    var $this = $(this);
+    var item = $this.closest('.line-list-item');
+    item.removeClass('startActive');
+    item.addClass('endActive');
+    lineObj.zd = item.find('.mid').text();
+    if ($('.endActive').length && $('.startActive').length) {
+      mainView.router.load({
+        url: 'line-detail.html'
+      })
+    }
+  });
+
+});
 //js-open-card
 // Generate dynamic page
 var dynamicPageIndex = 0;
